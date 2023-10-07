01c43aedf511e14bb24c9f99a658e3cfc79d36ba
matheus <matheus.resteves@gmail.com>
2023-10-07 20:44:03 -0300

    feat: botão para ver de jogos escolhidos

---
+++ src/components/ViewSelectedGamesButton/index.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./style";
+
+function ViewSelectedGamesButton () {
+    return (
+        <>
+        <Button>Jogos selecionados</Button>
+        </>
+    );
+}
+
+export default ViewSelectedGamesButton;
---
+++ src/components/ViewSelectedGamesButton/style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    width: 200px;
+    color: #000;
+    background-color: #A749E0;
+    font-size: 1em;
+    margin: 1em;
+    border: 2px solid #000;
+    border-radius: 11px;
+    padding: 1em;
+`;
+
+export { Button };
