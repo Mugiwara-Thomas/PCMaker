6752db9e094303567f6ff0b2a8de932b7c752f1a
matheus <matheus.resteves@gmail.com>
2023-10-10 17:14:42 -0300

    style: mudança de qtde de linhas em nav-2 e main

--- src/styles/GridApp.jsx
+++ src/styles/GridApp.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const WrapperGeneral = styled.div`
   display: grid;
   height: 100vh;
-  grid-template-rows: 0.2fr 1fr 0.5fr 0.5fr 0.9fr;
+  grid-template-rows: 0.2fr 1fr 20.0fr 15.0fr 0.9fr;
   grid-template-areas:
     "nav"
     "nav-2"
