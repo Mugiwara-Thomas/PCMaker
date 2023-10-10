375a9f34fb5c39cf82aaedb766e82b825ce556da
matheus <matheus.resteves@gmail.com>
2023-10-10 17:15:29 -0300

    feat: add elementos na página de response

--- src/pages/response.jsx
+++ src/pages/response.jsx
@@ -1,13 +1,28 @@
 import Nav from "../components/Navbar";
 import GlobalStyle from "../styles/GlobalStyle";
+import FooterBar from "../components/Footer";
+import Painel from "../components/PainelGames";
+import Card from "../components/Card";
+import { WrapperGeneral } from "../styles/GridApp"
+import {BoxViewOver, BoxViewUnder} from "../components/BoxView";
+
 
 function Response() {
   return (
-    <>
+    <WrapperGeneral>
       <GlobalStyle />
-      <Nav/>
-      <p>Sou a tela de Response</p>
-    </>
+      <Nav  />
+      <BoxViewOver width = {85} height = {100}>
+      </BoxViewOver>
+      <BoxViewUnder width = {85} height = {90}>
+        <Painel response={true}>
+          <Card endereco={"https://img.ibxk.com.br/2018/05/03/red-dead-03201115279005.jpg?ims=328x"}/>
+          <Card endereco={"https://m.media-amazon.com/images/I/91Aa9ZIAaoL._AC_UF1000,1000_QL80_.jpg"}/>
+          <Card endereco={"https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/1280/V.jpg"}/>
+        </Painel>
+      </BoxViewUnder>
+      <FooterBar />
+    </WrapperGeneral>
   );
 }
 
