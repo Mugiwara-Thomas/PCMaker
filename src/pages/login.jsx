0f1b4290be087faf328380d86dda9dde09bc5804
matheus <matheus.resteves@gmail.com>
2023-10-24 09:26:33 -0300

    feat: add alguns components da página de login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,19 @@
 import Nav from "../components/Navbar";
 import GlobalStyle from "../styles/GlobalStyle";
+import { BoxViewOver } from "../components/BoxView";
+import InputBox from "../components/InputBox";
+import Button from "../components/Button";
 
 function Login() {
   return (
     <>
       <GlobalStyle />
       <Nav/>
-      <p>Sou a tela de login</p>
+      <BoxViewOver>
+        <InputBox descricao="Login"/>
+        <InputBox descricao="Password"/>
+        <Button />
+      </BoxViewOver>
     </>
   );
 }
