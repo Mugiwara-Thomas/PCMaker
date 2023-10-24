06ef4d87a661d17eef8bf58ccdc66849a3238195
matheus <matheus.resteves@gmail.com>
2023-10-24 09:32:47 -0300

    style: identação de codigo

--- src/components/SelectionGames/index.jsx
+++ src/components/SelectionGames/index.jsx
@@ -3,12 +3,12 @@ import { BsBox2 } from 'react-icons/bs';
 
 function SelectionGames () {
     const numeroDeJogos = 10;
+
     return (
         <Wrapper>
             <Input placeholder="Digite o nome do game"/>
             <Button>
                 <Wrapper>
-
                 <BsBox2 size={30}/>
                 <NumeroDeJogos>
                     {numeroDeJogos}
