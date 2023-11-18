26b2688af931ad84e4c29639cc59e541615512e1
matheus <matheus.resteves@gmail.com>
2023-11-18 19:23:19 -0300

    Refactor: mudança do card para mostrar as imagens dos jogos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable react/prop-types */
-import { CardPai, CardFilho } from './style';
+import { CardPai, CardFilho, GameName } from './style';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../../features/marketingCart/marketingCart";
 
-function Card({endereco}) {
+function Card({endereco, nome, ID}) {
+  const dispatch = useDispatch()
+  const jogos = useSelector((state) => state.carrinho.lista)
+  const handleClick = () => {
+    const jogoExistente = jogos.find(jogo => jogo === ID);
+    if (jogoExistente) {
+      dispatch(decrement(ID)) 
+    } else {
+      dispatch(increment(ID))
+    }
+  }
+
   return (
     <>
-      <CardPai>
-        <CardFilho url={endereco}></CardFilho>
+      <CardPai onClick={handleClick}>
+        <CardFilho url={endereco}>
+          <GameName>{nome}</GameName> 
+        </CardFilho>
       </CardPai>
     </>
   );
